6065efc65680d72a9f7f02e7c30ea90c64bc3ed6
carrabre <40183833+carrabre@users.noreply.github.com>
2021-03-24 10:40:19 -0700

    Add files via upload

---
+++ configTwit.js
@@ -0,0 +1,8 @@
+module.exports = {
+    consumer_key:         '2BGUzVFnie4HDJ9pxvLifkybO',
+    consumer_secret:      'CSExVkXO4ejVRh9nPRNrb31Nf9Of9E9wY8pd0mzl7tO0u60e0x',
+    access_token:         '1372055070565494785-n0eFUHeOlU8IcQlbccqgcfbjf2FN9n',
+    access_token_secret:  'hfGR9MBi10KL4YwiXVj5WOSaZS4aXeakXNVCnnNMBppfK',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    //strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
